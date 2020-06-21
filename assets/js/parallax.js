b9a6bae3175c3cb8640412e89adb1b837db321b4
Sarah <vanwars@gmail.com>
2020-06-20 19:41:09 -0500

    parallax bugfix

---
+++ assets/js/parallax.js
@@ -0,0 +1,13 @@
+/*
+const p1 = document.querySelector('.parallax-one');
+const p2 = document.querySelector('.parallax-two');
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+
+console.log(vw, vh);
+
+
+p1.style.backgroundSize = `${vh}px ${vh}px`;
+p2.style.backgroundSize = `${vh}px ${vh}px`;
+p2.style.backgroundPosition = `${vw / 2}px 55px`;
+*/
